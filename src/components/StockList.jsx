7780683e78716462f59c17648f0fa15c6200207b
Sameer Chand <sam@manishas-MacBook-Air.local>
2025-04-01 11:28:19 +0530

    added pagination for the stocks list

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import StockCard from "./StockCard";
 
 const StockList = ({ stocks, sortFilter }) => {
+  const [stockCount, setStockCount] = useState(10);
   return (
-    <div className="flex flex-col">
-      {stocks.map((stock) => (
+    <div className="flex flex-col mb-10">
+      {stocks.slice(0, stockCount).map((stock) => (
         <StockCard key={stock._id} stock={stock} sortFilter={sortFilter} />
       ))}
+      <div className="py-2 border-y-1 border-[#dde0e4]">
+        <button
+          className="w-[100%] py-5 text-blue-500  hover:bg-gray-200 rounded "
+          onClick={() => {
+            if (stockCount < stocks.length) setStockCount(stockCount + 10);
+          }}
+        >
+          Load more smallcases
+        </button>
+      </div>
     </div>
   );
 };
